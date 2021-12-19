d8474e79d35c27a940f44aeb8d7bb8f85838edb4
alk3pInjection <webmaster@raspii.tech>
2021-12-19 12:34:47 +0800

    qcacld-3.0: Fix build
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
@@ -15333,7 +15333,9 @@ int hdd_init(void)
 
 	hdd_trace_init();
 	hdd_register_debug_callback();
+#ifdef FEATURE_ROAM_DEBUG
 	wlan_roam_debug_init();
+#endif
 
 	return 0;
 }
@@ -15347,7 +15349,9 @@ int hdd_init(void)
  */
 void hdd_deinit(void)
 {
+#ifdef FEATURE_ROAM_DEBUG
 	wlan_roam_debug_deinit();
+#endif
 
 #ifdef WLAN_LOGGING_SOCK_SVC_ENABLE
 	wlan_logging_sock_deinit_svc();
